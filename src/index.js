57d7887682c0fdd0218ebff1cdc6cc3b40342630
Bernardo Mafra <bernardo.mafra@sga.pucminas.br>
2021-05-16 15:04:19 -0300

    refactor: now checking empty data for all columns

--- src/index.js
+++ src/index.js
@@ -7,13 +7,11 @@ const Normalize = require('./utils/normalize')
 const CSV = require('./utils/csv')
 
 function checkAndLogEmptyValuesCount(arrayWithCsvData) {
-    let linesWithoutCountry = Normalize.getEmptyRowsByKeyInArray('Country', arrayWithCsvData)
-    let linesWithoutDateAdded = Normalize.getEmptyRowsByKeyInArray('Date Added', arrayWithCsvData)
-    let linesWithoutRating = Normalize.getEmptyRowsByKeyInArray('Rating', arrayWithCsvData)
-
-    Logger.keyDataCountOfTotal('Coutry', linesWithoutCountry.length, arrayWithCsvData.length)
-    Logger.keyDataCountOfTotal('Date Added', linesWithoutDateAdded.length, arrayWithCsvData.length)
-    Logger.keyDataCountOfTotal('Rating', linesWithoutRating.length, arrayWithCsvData.length)
+    const columns = CSV.getColumns()
+    columns.forEach(column => {
+        const linesWithoutThisColumn = Normalize.getEmptyRowsByKeyInArray(column.title, arrayWithCsvData)
+        Logger.keyDataCountOfTotal(column.title, linesWithoutThisColumn.length, arrayWithCsvData.length)
+    })
 }
 
 csvToJson({ output: "line", noheader: false })
